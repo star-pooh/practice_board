01c0495e51ca4779c8448e66d7b1ad731e08016f
star-pooh <eunki92@gmail.com>
2024-12-12 11:49:20 +0900

    Init : Create practice_board 게시판 실습 생성

---
+++ src/main/java/org/example/practice_board/PracticeBoardApplication.java
@@ -0,0 +1,13 @@
+package org.example.practice_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBoardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticeBoardApplication.class, args);
+  }
+
+}
